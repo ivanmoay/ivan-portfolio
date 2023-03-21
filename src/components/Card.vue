6358b8a7417d2bd1882d3ef40848cfdc8a4f9658
ivanmoay <37124599+ivanmoay@users.noreply.github.com>
2023-03-21 22:00:51 +0800

    fixed logos and tooltip

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,57 +6,75 @@
             <img src="../assets/linkedIn.png" alt="linkedIn" class="w-6 h-6">
             <img src="../assets/linkedIn.png" alt="linkedIn" class="w-6 h-6"> -->
             <div v-for="tool in tools">
-               <div v-if="tool == 'laravel'">                    
+               <div v-if="tool == 'laravel'" class="flex gap-2 group">                    
                     <img 
                         src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Laravel.svg" 
                         alt="Laravel" 
                         class="w-6 h-6 hover:cursor-pointer" 
-                        data-tooltip-target="tooltip-laravel"
                     >
-                    <div id="tooltip-laravel" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                        Laravel
-                        <div class="tooltip-arrow" data-popper-arrow></div>
-                    </div>
+                    <span class="bg-gray-800 text-white px-2 rounded-lg invisible group-hover:visible absolute mt-7 ml-[-20px]">Laravel</span>
                </div>    
-               <div v-if="tool == 'bootstrap'">                    
+               <div v-if="tool == 'bootstrap'" class="flex gap-2 group">                    
                     <img 
                         src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg" 
                         alt="Bootstrap" 
                         class="w-6 h-6 hover:cursor-pointer" 
-                        data-tooltip-target="tooltip-bootstrap"
                     >
-                    <div id="tooltip-bootstrap" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                        Bootstrap
-                        <div class="tooltip-arrow" data-popper-arrow></div>
-                    </div>
-               </div>    
+                    <span class="bg-gray-800 text-white px-2 rounded-lg invisible group-hover:visible absolute mt-7 ml-[-20px]">Bootstrap</span>
+               </div>  
+               <div v-if="tool == 'vuejs'" class="flex gap-2 group">                    
+                    <img 
+                        src="https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg" 
+                        alt="Vue.js" 
+                        class="w-6 h-6 hover:cursor-pointer" 
+                    >
+                    <span class="bg-gray-800 text-white px-2 rounded-lg invisible group-hover:visible absolute mt-7 ml-[-15px]">Vue.js</span>
+               </div> 
+               <div v-if="tool == 'tailwind'" class="flex gap-2 group">                    
+                    <img 
+                        src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg" 
+                        alt="Tailwind CSS" 
+                        class="w-6 h-6 hover:cursor-pointer" 
+                    >
+                    <span class="bg-gray-800 text-white px-2 rounded-lg invisible group-hover:visible absolute mt-7 ml-[-40px]">Tailwind CSS</span>
+               </div>                  
             </div>
         </div>
         <div class="text-gray-700 text-2xl mb-2">{{ contentText }}</div>
         <div class="flex space-x-2 justify-center mb-2">
-            <a href="props.link">
+            <a :href="mGitHub" target="_blank">
+                <img src="../assets/github-mark/github-mark.png" alt="linkedIn" class="w-8 h-8">
+            </a>
+            <a :href="mLink" target="_blank" v-if="mLink">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                 </svg>
-            </a>
-            <a href="props.github">
-                <img src="../assets/github-mark/github-mark.png" alt="linkedIn" class="w-8 h-8">
-            </a>
+            </a>            
         </div>
     </div>
 </template>
 
 <script>
+import { onMounted,ref } from 'vue';
+
 export default{
     props: {
         header: String,
         contentText: String,
         github: String,
         link: String,
-        tools: [],
+        tools: Array,
     },
-    // setup(props){
+    setup(props){
+        let mLink = ref('')
+        let mGitHub = ref('')
 
-    // }
+        onMounted(()=>{
+            mLink.value = props.link
+            mGitHub.value = props.github
+        })
+
+        return { mLink, mGitHub }
+    }
 }
 </script>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -3,33 +3,48 @@
         <Card 
             header="Material Management Office Monitoring System" 
             :tools="['laravel','bootstrap']"
-            github="www.google.com"
-            link="www.google.com"
+            github="https://github.com/ivanmoay/djrmh-mmo"
+            link=""
             contentText="An ongoing project for the MMO office used for keeping track of inventory, purchase orders, and
                         requisitions. I’m responsible for developing the entire system using Laravel and NiceAdmin(bootstrap
                         template)."            
         />
         <Card 
             header="Portfolio" 
+            :tools="['vuejs','tailwind']"
+            github="https://github.com/ivanmoay/ivan-portfolio"
+            link="https://gochirodev.com/"
             contentText="A personal web application showcasing my resume and portfolio using Vue.js and TailwindCSS."
         />
         <Card 
             header="Etecon App" 
+            :tools="['laravel','tailwind']"
+            github="https://github.com/ivanmoay/etecon-app"
+            link=""
             contentText="A web application made for a client mainly to generate hospice forms. The client can also make video
                         courses with an exam after completion of a course."
         />
         <Card 
             header="Appointment Web Application" 
+            :tools="['laravel','bootstrap']"
+            github="https://github.com/ivanmoay/djrmh-appointment"
+            link=""
             contentText="Web application made to manage Out Patient bookings. Hospital staff can create services with daily
                         schedules. A client portal for patients is also available."
         />
         <Card 
             header="Kam-Inn Room Monitoring App" 
+            :tools="['laravel','bootstrap']"
+            github="https://github.com/ivanmoay/kam-inn"
+            link=""
             contentText="A simple web application used to monitor room rentals and inventory purchase. The application also
                         emails the Inn owner for every transaction."
         />
         <Card 
             header="Notes App" 
+            :tools="['laravel','vuejs','tailwind']"
+            github="https://github.com/ivanmoay/notes-app"
+            link=""
             contentText="A CRUD web application made with Laravel, Vue.js and TailwindCSS."
         />
     </div>
